770012b428cc1f5fdcba42b1c973c2b33ed0d007
huangzijian888 <huangzijian888@gmail.com>
2019-08-11 14:06:22 +0800

    初始化

---
+++ src/main/java/cn/huangzijian888/wechatpay/WechatpayApplication.java
@@ -0,0 +1,13 @@
+package cn.huangzijian888.wechatpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatpayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WechatpayApplication.class, args);
+    }
+
+}
