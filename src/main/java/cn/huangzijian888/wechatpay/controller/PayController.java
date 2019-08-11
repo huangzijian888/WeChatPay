72931c356dba528a385d94c9ff01ff1db0026271
huangzijian888 <huangzijian888@gmail.com>
2019-08-11 22:18:25 +0800

    修复Bug key注入漏写}

--- src/main/java/cn/huangzijian888/wechatpay/controller/PayController.java
+++ src/main/java/cn/huangzijian888/wechatpay/controller/PayController.java
@@ -6,7 +6,6 @@ import cn.hutool.crypto.digest.DigestAlgorithm;
 import cn.hutool.crypto.digest.Digester;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
